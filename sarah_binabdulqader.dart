a8eb8b558392948b9525d947a5f39e97999690c2
sarahsaleh112 <sara03919@gmail.com>
2024-12-04 14:52:11 +0300

    Add files via upload

---
+++ sarah_binabdulqader.dart
@@ -0,0 +1,9 @@
+import 'package:sarah_binabdulqader/sarah_binabdulqader.dart' as sarah_binabdulqader;
+
+int add(int a , int b){
+  return a+b;
+}
+void main() {
+  int result = add(3,5);
+  print("the sum is : $result");
+}
